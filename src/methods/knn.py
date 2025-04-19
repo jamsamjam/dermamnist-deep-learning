c9fdf8a20467104afdfd27ac8f114d091bf496a2
cmaisonn <clea.maisonnier@epfl.ch>
2025-04-19 17:32:42 +0200

    knn fixes

--- src/methods/knn.py
+++ src/methods/knn.py
@@ -11,7 +11,7 @@ class KNN(object):
         """
         self.k = k
         self.task_kind = task_kind
-
+       
     def fit(self, training_data, training_labels):
         """
             Trains the model, returns predicted labels for training data.
@@ -26,9 +26,17 @@ class KNN(object):
             Returns:
                 pred_labels (np.array): labels of shape (N,)
         """
+
+        #Save the training_data and training_labels as part of the class
         self.training_data = training_data
-        self.training_labels= training_labels
-        return training_labels
+        self.training_labels= training_labels.astype(int)
+        
+        #Call the predict function
+        pred_labels = self.predict(training_data)
+        
+        return pred_labels
+    
+    
     
     def predict(self, test_data):
         """
@@ -39,38 +47,54 @@ class KNN(object):
             Returns:
                 test_labels (np.array): labels of shape (N,)
         """
-        predictions = []
+        list = []
         for val in test_data:
-            predictions.append(self._kNN_predict_one(val))
-        test_labels = np.array(predictions)
+            list.append(self.kNN_predict_one(val))
+            
+        test_labels = np.array(list)
         
         return test_labels
     
-    def _euclidean_dist(self,example):
+    
+    def euclidean_dist(self,example):
         """Compute the Euclidean distance between a single example
         vector and all training_examples.
         """
+  
         sum = np.sum((self.training_data - example)**2, axis=1)
+
         return np.sqrt(sum)
 
-    def _find_k_nearest_neighbors(self, distances):
+
+    def find_k_nearest_neighbors(self, distances):
         """ Find the indices of the k smallest distances from a list of distances.
         """
+
         sorted_indices = np.argsort(distances)
+        
         return sorted_indices[:self.k]
 
-    def _predict_label(self,neighbor_labels):
+
+    def predict_label(self,neighbor_labels):
         """Return the most frequent label in the neighbors'.
         """
+     
         counts = np.bincount(neighbor_labels.astype(int))
+
         return np.argmax(counts)
 
-    def _kNN_predict_one(self,unlabeled_example):
+    
+    def kNN_predict_one(self,unlabeled_example):
         """Returns the label of a single unlabelled example.
         """
-        distances = self._euclidean_dist(unlabeled_example)
-        nn_indices = self._find_k_nearest_neighbors(distances)
+        
+        distances = self.euclidean_dist(unlabeled_example)
+ 
+        nn_indices = self.find_k_nearest_neighbors(distances)
+
         neighbor_labels = self.training_labels[nn_indices]
-        best_label = self._predict_label(neighbor_labels) 
+
+        best_label = self.predict_label(neighbor_labels) 
+        
 
         return best_label
